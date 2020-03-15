11d7db2f0c3ab594709423eccc94e9cab68d7ce6
Víctor LV <bigtor@pop-os.localdomain>
2020-03-15 15:06:14 +0100

    use food object in both get and post methods

--- src/food.rs
+++ src/food.rs
@@ -1,10 +1,22 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{web, get, post, Responder};
+use serde::{Serialize, Deserialize};
 
+#[derive(Serialize, Deserialize)]
 pub struct Food {
     name: String,
 }
 
 #[get("/food")]
-async fn foods() -> impl Responder {
-    HttpResponse::Ok().body("asdf")
+async fn get_foods() -> impl Responder {
+    web::Json(
+        vec![
+            Food { name: "apple".to_owned()},
+            Food { name: "pear".to_owned()}
+        ]
+    )
+}
+
+#[post("/food")]
+async fn post_food(food: web::Json<Food>) -> impl Responder {
+    String::from(&food.name)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod food;
 async fn main() -> std::io::Result<()> {
     let mut listenfd = ListenFd::from_env();
     let mut server = HttpServer::new(|| {
-        App::new().service(food::foods)
+        App::new().service(food::get_foods).service(food::post_food)
     });
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)?
